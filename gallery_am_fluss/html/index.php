941ab849d455c64bc8a97ac50141df73b79f2076
Linus Bittner <2130@htl.rennweg.at>
2025-10-18 22:24:10 +0200

    us7 done

--- gallery_am_fluss/html/index.php
+++ gallery_am_fluss/html/index.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . "/php/classes/Painting.php";
+require_once __DIR__ . "/php/classes/Sculpture.php";
 
 $paintings = [
     new Painting("Sunset Over the River", "Clara Meier", "media/art-gallery-pic-1.jpg"),
@@ -7,6 +8,13 @@ $paintings = [
     new Painting("Abstract Fields", "Sofia Müller", "media/art-gallery-pic-3.jpg"),
     new Painting("Golden Reflections", "Jonas Becker", "media/art-gallery-pic-4.jpg"),
     new Painting("Silent Horizon", "Emma Scholz", "media/art-gallery-pic-5.jpg"),
+];
+$sculptures = [
+    new Sculpture("Stone Embrace", "Max Schmidt", "media/art-gallery-sculpture-1.jpg"),
+    new Sculpture("Celestial Spiral", "Lena Wagner", "media/art-gallery-sculpture-2.jpg"),
+    new Sculpture("Iron Blossom", "Noah Fischer", "media/art-gallery-sculpture-3.jpg"),
+    new Sculpture("The Observer", "Julia Keller", "media/art-gallery-sculpture-4.jpg"),
+    new Sculpture("Rhythm of the Void", "Elias Hoffmann", "media/art-gallery-sculpture-5.jpg"),
 ];
 ?>
 <!DOCTYPE html>
@@ -21,6 +29,13 @@ $paintings = [
     <div id="gallery">
         <?php foreach ($paintings as $painting): ?>
             <?= $painting->getDisplayHTML() ?>
+        <?php endforeach; ?>
+    </div>
+
+    <h1>Gallery Am Fluss userStory 7 Sculptures</h1>
+    <div id="gallery">
+        <?php foreach ($sculptures as $sculpture): ?>
+            <?= $sculpture->getDisplayHTML() ?>
         <?php endforeach; ?>
     </div>
 </body>
