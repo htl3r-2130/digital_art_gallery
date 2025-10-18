bab2d87ceaaae53dd3594235b05bb8452d81173e
AlessandroTervit <alessandrotervit@gmail.com>
2025-10-18 23:30:33 +0200

    php updated

--- gallery_am_fluss/html/php/classes/Painting.php
+++ gallery_am_fluss/html/php/classes/Painting.php
@@ -3,24 +3,34 @@ require_once __DIR__ . '/../interfaces/ArtworkInterface.php';
 
 class Painting implements ArtworkInterface {
 
-  public function __construct(private string $title, private string $artist, private string $imagePath) {}
+    public function __construct(
+        private string $title,
+        private string $artist,
+        private string $imagePath,
+        private string $description = ""  // optional
+    ) {}
 
-  public function getName(): string {
-      return $this->title;
-  }
+    public function getName(): string {
+        return $this->title;
+    }
 
-  public function getArtist(): string {
-      return $this->artist;
-  }
+    public function getArtist(): string {
+        return $this->artist;
+    }
 
-  public function getDisplayHTML(): string {
-      return "
-        <div class='image'>
-          <img src='./{$this->imagePath}' alt=''>
-          <div class='description'>
-            <h1>{$this->title} - {$this->artist}</h1>
-          </div>
-        </div>
-      ";
-  }
+    public function getDisplayHTML(): string {
+        $smallImagePath = str_replace("media/", "media/small/", $this->imagePath);
+
+        $html = '<div class="image" style="background-image: url(\'' . $smallImagePath . '\');">';
+        $html .= '<div class="backdrop-layer">';
+        $html .= '<img src="' . $this->imagePath . '" alt="">';
+        $html .= '<div class="description">';
+        $html .= '<h1>' . htmlspecialchars($this->title) . ' - ' . htmlspecialchars($this->artist) . '</h1>';
+        if (!empty($this->description)) {
+            $html .= '<p>' . htmlspecialchars($this->description) . '</p>';
+        }
+        $html .= '</div></div></div>';
+
+        return $html;
+    }
 }
