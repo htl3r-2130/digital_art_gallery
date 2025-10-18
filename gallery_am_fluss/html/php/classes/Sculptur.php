0ebac1d101e5eafe83e573332af097437d10f2ca
Linus Bittner <2130@htl.rennweg.at>
2025-10-18 22:14:35 +0200

    Sculpture.php added

---
+++ gallery_am_fluss/html/php/classes/Sculptur.php
@@ -0,0 +1,26 @@
+<?php
+require_once __DIR__ . '/../interfaces/ArtworkInterface.php';
+
+class Sculptur implements ArtworkInterface {
+
+  public function __construct(private string $title, private string $artist, private string $imagePath) {}
+
+  public function getName(): string {
+      return $this->title;
+  }
+
+  public function getArtist(): string {
+      return $this->artist;
+  }
+
+  public function getDisplayHTML(): string {
+      return "
+        <div class='image'>
+          <img src='./{$this->imagePath}' alt=''>
+          <div class='description'>
+            <h1>{$this->title} - {$this->artist}</h1>
+          </div>
+        </div>
+      ";
+  }
+}
