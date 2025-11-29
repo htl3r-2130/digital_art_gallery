91d48478cf5ec37408a04a58c19de8ce22e871ca
Linus Bittner <2130@htl.rennweg.at>
2025-11-29 12:45:34 +0100

    C4 us7

--- public/index.php
+++ public/index.php
@@ -1,22 +1,54 @@
 <?php
+require __DIR__ . "/../vendor/autoload.php";
+
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\Writer\PngWriter;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\RoundBlockSizeMode;
+
+$writer = new PngWriter();
 $template = fread(fopen("template.html", "r"), filesize("index.html"));
 
 $artworks = [
-    ["media/art-gallery-pic-1.jpg", "Sunset River (2001)"],
-    ["media/art-gallery-pic-2.jpg", "Mountain Dreams (1998)"],
-    ["media/art-gallery-pic-3.jpg", "Ocean Reflections (2015)"]
+    ["media/art-gallery-pic-1.jpg", "Sunset River (2001)", "Sunset River (2001) – Famous river landscape painting"],
+    ["media/art-gallery-pic-2.jpg", "Mountain Dreams (1998)", "Mountain Dreams (1998) – Abstract mountain scenery"],
+    ["media/art-gallery-pic-3.jpg", "Ocean Reflections (2015)", "Ocean Reflections (2015) – Modern ocean reflections"]
 ];
 
 $artworkText = "";
 foreach ($artworks as $artwork) {
-  $artworkText .= <<<HTML
+
+    $builder = new Builder(
+        writer: new PngWriter(),
+        data: $artwork[2],
+        encoding: new Encoding('UTF-8'),
+        errorCorrectionLevel: ErrorCorrectionLevel::High,
+        size: 150,
+        margin: 10,
+        roundBlockSizeMode: RoundBlockSizeMode::Margin
+    );;
+    $result = $builder->build();
+    $slug = slugify($artwork[1]);
+    $qrFile = "media/qr/qr-$slug.png";
+    $result->saveToFile($qrFile);
+
+    $artworkText .= <<<HTML
         <div class='artwork'>
             <img src="{$artwork[0]}" alt="painting">
             <div class='description'>
                 <h1>{$artwork[1]}</h1>
+                <img class="qr" src="$qrFile" alt="QR Code">
             </div>
         </div>
     HTML;
-} 
+}
 $render = str_replace("{{Artworks}}", $artworkText, $template);
 echo $render;
+
+function slugify($text)
+{
+    $text = strtolower(trim($text));
+    $text = preg_replace('/[^a-z0-9]+/i', '-', $text);
+    return trim($text, '-');
+}
